ecf7368c2344dc1dfd1b4b0b03ab5b0696954132
AverageDemo <46252711+AverageDemo@users.noreply.github.com>
2021-05-20 21:28:22 -0400

    added some missing pages

--- client/pages/bugs/new.js
+++ client/pages/bugs/new.js
@@ -1,10 +1,10 @@
 import { API } from "@/config/index"
 import Layout from "@/components/Layout"
 
-export default function NewProjectPage() {
+export default function NewBugPage() {
     return (
-        <Layout breadcrumb="New Project">
-            <h1>This is where the new project form will be</h1>
+        <Layout breadcrumb="New Bug">
+            <h1>This is where the new bug form will be</h1>
         </Layout>
     )
 }
