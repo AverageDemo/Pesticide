2a62f7f0b237690131bb7e255b989d151d31497b
AverageDemo <46252711+AverageDemo@users.noreply.github.com>
2021-05-22 22:21:47 -0400

    and again... formatting

--- client/pages/bugs/[project]/[slug].js
+++ client/pages/bugs/[project]/[slug].js
@@ -240,18 +240,18 @@ export default function BugPage({ bug, projectObj }) {
                             {/* Only show this button if admin / lead dev */}
                             <button
                                 onClick={handleDeleteBtn}
-                                className="inline-flex justify-center rounded-md border border-transparent shadow-sm mr-2 px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                             >
                                 Delete
                             </button>
                             {bug.status === 0 ? (
-                                <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                                <button className="inline-flex justify-center ml-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
                                     Assign to user
                                 </button>
                             ) : bug.status === 1 ? (
                                 <button
                                     onClick={handleReviewBtn}
-                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="inline-flex justify-center ml-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
                                     Mark for Review
                                 </button>
